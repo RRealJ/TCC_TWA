31b6f3e899dc95d59ce883558631329fb4168078
RRealJ <renatorealjunior2@gmail.com>
2024-02-16 20:59:58 -0300

    Pause Menu

---
+++ Menus/menu_pausa.gd
@@ -0,0 +1,33 @@
+extends CanvasLayer
+
+@onready var btn_continuar = $menu_holder/btn_continuar
+
+func _ready():
+	visible = false
+
+
+func _on_btn_continuar_pressed():
+	get_tree().paused = false
+	visible = false
+
+
+func _on_btn_inventário_pressed():
+	pass # Replace with function body.
+
+
+func _on_btn_opcoes_pressed():
+	pass # Replace with function body.
+
+
+func _on_btn_main_menu_pressed():
+	pass # Replace with function body.
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pausar"):
+		visible = true
+		get_tree().paused = true
+		btn_continuar.grab_focus()
+		
+		
+		
