4bc665a6e03f83edb5cef26fa7ec8e6d4f053975
RRealJ <renatorealjunior2@gmail.com>
2024-02-16 01:49:18 -0300

    Slider de Volume, e mais menu de opções

--- Menu.gd
+++ Menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 func _on_comecar_pressed():
 	get_tree().change_scene_to_file("res://Mundos/mundo_1.tscn")
-
+	
 
 func _on_loja_pressed():
 	pass # Replace with function body.
@@ -15,3 +15,5 @@ func _on_sair_pressed():
 
 func _on_opcoes_pressed():
 	get_tree().change_scene_to_file("res://opt_menu.tscn")
+
+
--- opt_menu.gd
+++ opt_menu.gd
@@ -1,9 +1,17 @@
 extends Control
 
+var master_bus = AudioServer.get_bus_index("Master")
 
-func _on_volume_pressed():
-	pass # Replace with function body.
+func _on_volume_value_changed(valor):
+	AudioServer.set_bus_volume_db(master_bus, valor)
 	
+	if valor == -30:
+		AudioServer.set_bus_mute(master_bus, true)
+	else:
+		AudioServer.set_bus_mute(master_bus, false)
+
 
 func _on_voltar_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
+
+
