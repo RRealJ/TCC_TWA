88101df9cf6b6bec4bc2cd7ec24ea706b3caa050
RRealJ <renatorealjunior2@gmail.com>
2024-02-21 09:48:27 -0300

    Font Fix
    Consertado bug com Font PIxelada, onde a fonte ficava muito borrada em certas ocasiões.

--- Menus/Menu.gd
+++ Menus/Menu.gd
@@ -1,19 +1,22 @@
 extends Control
 
 
-func _on_comecar_pressed():
-	get_tree().change_scene_to_file("res://Mundos/mundo_1.tscn")
-	
 
-func _on_loja_pressed():
+
+func _on_btn_saltar_pressed():
+	get_tree().change_scene_to_file("res://Mundos/mundo_1.tscn")
+
+
+func _on_btn_loja_pressed():
 	pass # Replace with function body.
 
 
-func _on_sair_pressed():
+func _on_btn_opcoes_pressed():
+	get_tree().change_scene_to_file("res://Menus/opt_menu.tscn")
+
+
+func _on_btn_sair_pressed():
 	get_tree().quit()
 
-
-func _on_opcoes_pressed():
-	get_tree().change_scene_to_file("res://opt_menu.tscn")
 
 
