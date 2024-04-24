2c9924be742fdbf68d2dbecdfc5a8e001fec38cf
RRealJ <renatorealjunior2@gmail.com>
2024-04-24 13:59:38 -0300

    otimizações
    principalmente na loja e tela de confirmação.

--- loja.gd
+++ loja.gd
@@ -1,12 +1,9 @@
 extends Control
 
 @onready var btn_up_vida = $p_upgrades/set_1/up_vida
-@onready var upbar_vida = $p_upgrades/set_1/upbar_vida
-@onready var upbar_resistencia = $p_upgrades/set_1/upbar_resistencia
-@onready var upbar_velocidade = $p_upgrades/set_2/upbar_velocidade
-@onready var upbar_sorte = $p_upgrades/set_2/upbar_sorte
-@onready var upbar_dano = $p_upgrades/set_3/upbar_dano
-@onready var upbar_chance_critica = $p_upgrades/set_3/upbar_chance_critica
+@onready var btn_up_resistencia = $p_upgrades/set_1/up_resistencia
+@onready var btn_up_velocidade = $p_upgrades/set_2/up_velocidade
+@onready var btn_up_chance_critica = $p_upgrades/set_3/up_chance_critica
 @onready var confirmacao = $confirmacao
 
 
@@ -24,12 +21,36 @@ func confirmar():
 	await confirmacao.decidido
 	confirmacao.hide()
 
-	
-func _on_up_vida_pressed():
+
+func _on_button_pressed(selecao, button):
 	await confirmar()
-	btn_up_vida.grab_focus()
-	if confirmacao.opt == 1 and upbar_vida.value < 5:
-		upbar_vida.value += 1
+	if confirmacao.opt == 1 and button.value < 5:
+		button.value += 1
 	else:
-		pass	
+		pass
+	selecao.grab_focus()
+	
+
+
+func _on_up_vida_pressed():
+	_on_button_pressed(btn_up_vida, $p_upgrades/set_1/upbar_vida)
 		
+
+func _on_up_resistencia_pressed():
+	_on_button_pressed(btn_up_resistencia, $p_upgrades/set_1/upbar_resistencia)
+
+
+func _on_up_velocidade_pressed():
+	_on_button_pressed(btn_up_velocidade, $p_upgrades/set_2/up_velocidade)
+
+
+func _on_up_sorte_pressed():
+	_on_button_pressed($p_upgrades/set_2/up_sorte, $p_upgrades/set_2/upbar_sorte)
+	
+	
+func _on_up_dano_pressed():
+	_on_button_pressed($p_upgrades/set_3/up_dano, $p_upgrades/set_3/upbar_dano)
+
+
+func _on_up_chance_critica_pressed():
+	_on_button_pressed(btn_up_chance_critica, $p_upgrades/set_3/upbar_chance_critica)
